f16b26c54a95e5b74eef111c3c5bdedea16bc4a3
IcaroNunesdosSantos <icarodsantos46@gmail.com>
2023-09-14 14:04:46 -0300

    While

---
+++ altura.java
@@ -0,0 +1,11 @@
+public class altura {
+	public static void main(String[] args) {
+		double i=0, j=134, p=145;
+		while(p>=j) {
+			j = j + 2.5;
+			p = p + 2;
+			i++;
+		}
+		 System.out.println("joão demora "+ i +" anos para ficar maior que pedro");
+	}
+}
